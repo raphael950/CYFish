7f085f4b2fc4a77ad3e1064e77ee1f68d0e60211
Twiz0x_x <twizox@gmail.com>
2024-04-06 01:45:42 +0200

    generation des cases

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 #define WIDTH 9
 #define LENGTH 9
+
 
 typedef struct {
     char fish;
@@ -17,10 +19,35 @@ Box boxBuilder(char fish, char playerId) {
 }
 
 void generateFish(Box map[WIDTH][LENGTH], int minMonoPinguins) {
+    int monoPinguinCount = 0;
     for (int y = 0; y < WIDTH; ++y) {
         for (int x = 0; x < LENGTH; ++x) {
-
+            int random = rand() % 3 + 2;
+            map[y][x].fish = random;
+            if (random == 1) monoPinguinCount++;
         }
+    }
+
+    // Remove fishes if they are too many, disturbing pinguins spawn
+    while (monoPinguinCount < minMonoPinguins) {
+        int x = rand() % WIDTH, y = rand() % LENGTH;
+        if (map[y][x].fish != 1) {
+            map[y][x].fish = 1;
+            monoPinguinCount++;
+        }
+    }
+}
+
+char penguinsNumber(char playerNumber) {
+    switch (playerNumber) {
+        case 2:
+            return 4;
+        case 3:
+            return 3;
+        case 4:
+            return 2;
+        default:
+            return 1;
     }
 }
 
@@ -29,18 +56,22 @@ typedef struct {
 } Penguin;
 
 int main() {
-
     Box map[WIDTH][LENGTH];
 
-    // demander nb joueur
-    // remplir les poissons (en fonction nb pingouins)
-    int players = 3;
 
+
+    // TODO: ask player number >= 2 && <= 6
+    int players = 6;
+
+    // Cases initialization
     for (int y = 0; y < LENGTH; ++y) {
         for (int x = 0; x < WIDTH; ++x) {
             map[y][x] = boxBuilder(0, -1);
         }
     }
+
+
+    generateFish(map, penguinsNumber(players)*players);
 
     for (int y = 0; y < LENGTH; ++y) {
         for (int x = 0; x < WIDTH; ++x) {
@@ -50,6 +81,5 @@ int main() {
     }
 
 
-    free(map);
     return 0;
 }
